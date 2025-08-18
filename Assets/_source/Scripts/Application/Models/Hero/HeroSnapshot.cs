ec125d302c85829e8e7ae5017c415195fd495a8f
Neoxider <xotrios@gmail.com>
2025-08-18 19:18:31 +0500

    Refactoring

---
+++ Assets/_source/Scripts/Application/Models/Hero/HeroSnapshot.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Models.Hero
+{
+    public sealed class HeroSnapshot
+    {
+        public string Name { get; }
+        public int Level { get; }
+        public int Strength { get; }
+
+        public HeroSnapshot(string name, int level, int strength)
+        {
+            Name = string.IsNullOrWhiteSpace(name) ? "Hero" : name;
+            Level = level < 1 ? 1 : level;
+            Strength = strength < 0 ? 0 : strength;
+        }
+    }
+}
---
+++ Assets/_source/Scripts/Application/Ports/Hero/IHeroRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Ports.Hero
+{
+    using Domain.Models.Hero;
+
+    public interface IHeroRepository
+    {
+        Hero Get();
+        void Save(Hero hero);
+    }
+}
